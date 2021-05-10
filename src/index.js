bed43513d44af9987152283dca254b4bc8052fe2
Legotin Anton <ant.legotin@yandex.ru>
2021-05-10 16:11:43 +0500

    lesson2

--- src/index.js
+++ src/index.js
@@ -7,28 +7,3 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
   );
-
-
-
-// let messages = ['Привет', 'Как дела'];
-
-
-// const MessageComponent = (prop) => <div>{prop.text}</div>;
-
-// const MessageField = (props) => {
-//    return props.messages.map(message => <MessageComponent text={ message } />);
-// };
-
-// const MessageBtn = () => {
-//    return <button onClick={() => {messages.push('Нормально'); rend()}} >Отправить</button>
-// }
-// let rend = function() {
-//    ReactDOM.render(
-//       <div>
-//          <MessageField messages={ messages } />
-//          <MessageBtn />
-//       </div>,
-//       document.getElementById('root'),
-//    );
-// }
-// rend();
