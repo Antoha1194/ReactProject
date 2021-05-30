da25f06d13f9d34386378d2fa343478bf8f2f75d
Legotin Anton <ant.legotin@yandex.ru>
2021-05-30 15:38:15 +0500

    lesson7

--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -8,16 +8,38 @@ export const addMessage = (text, author) => ({
     text,
     author
 })
+// export const fechMessage = (message, ) => {
+//         let xml = `<chat application='7478065272013953235' instance='165'><message>${message}</message></chat>`;
+//         fetch("https://www.botlibre.com/rest/api/chat", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": 'application/xml'
+//             },
+//             body: xml
+
+//         })
+//     }
 export const addMessageWithThunk = (text, author, chatId) => (dispatch, getState) => {
+    const parser = new DOMParser()
         dispatch(addMessage(text, author));
         dispatch(chatAddMessage(chatId, Object.keys(getState().messages).length));
         if(author !== AUTHOR.BOT ){
-            setTimeout(() => {
-                dispatch(addMessage("I am BOT", AUTHOR.BOT));
+            let xml = `<chat application='7478065272013953235' instance='165'><message>${text}</message></chat>`;
+            fetch("https://www.botlibre.com/rest/api/chat", {
+                method: "POST",
+                headers: {
+                    "Content-Type": 'application/xml'
+                },
+                body: xml
+
+            }).then(res => res.text()).then(res => {
+                const xml  = parser.parseFromString(res, "application/xml");
+                const domElnameMessage = xml.getElementsByTagName('message')[0];
+                console.log(domElnameMessage.textContent);
+                dispatch(addMessage(domElnameMessage.textContent, AUTHOR.BOT));
                 dispatch(chatAddMessage(chatId, Object.keys(getState().messages).length));
                 dispatch(chatSignalMessage(chatId));
                 setTimeout(() => dispatch(chatSignalMessage(chatId)), 1000);
-            }, 1000);
+            }).catch(err => console.log("err", err));
         }  
-      
     };
